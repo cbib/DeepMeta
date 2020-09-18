b1bd966fd208c733132748577a964909d813ff26
Edgar <lefevreedg@gmail.com>
2020-09-18 11:11:29 +0200

    try to add tests

--- test/main_test.py
+++ test/main_test.py
@@ -2,6 +2,6 @@ import DeepMetav4.utils.data as data
 import DeepMetav4.utils.global_vars as gv
 
 
-def data_shape_test():
+def test_data_shape():
     dataset, label = data.create_dataset_detect(gv.path_img, gv.tab, gv.numSouris, 128)
     assert dataset[0].shape == (128, 128, 1)
