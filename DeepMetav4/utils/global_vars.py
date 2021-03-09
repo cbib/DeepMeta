5df43999e5eeed296650c9acd08d2fbb4040420b
Edgar <lefevreedg@gmail.com>
2021-03-09 16:21:20 +0100

    new path for detect

--- DeepMetav4/utils/global_vars.py
+++ DeepMetav4/utils/global_vars.py
@@ -3,12 +3,7 @@
 
 import os
 
-import pandas as pd
-
 import DeepMetav4.models.model_detection
-import DeepMetav4.models.model_resnet
-import DeepMetav4.models.model_unet
-import DeepMetav4.models.model_vgg16
 import DeepMetav4.models.small_plus_plus
 import DeepMetav4.utils.utils
 
@@ -23,51 +18,51 @@ PATH_SAVE = "data/saved_models/"
 PATH_RES = "data/results/"
 PATH_FILTERS_FEATURES = os.path.join(PATH_RES, "Filters_and_features/")
 
-PATH_CLASSIF = os.path.join(PATH_DATA, "Classif/")
+# PATH_CLASSIF = os.path.join(PATH_DATA, "Classif/")
 PATH_LUNGS = os.path.join(PATH_DATA, "Poumons/")
-PATH_META = os.path.join(PATH_DATA, "Metastases/")
+# PATH_META = os.path.join(PATH_DATA, "Metastases/")
 NEW_PATH_META = os.path.join(PATH_DATA, "Metastases_new/")
-OLD_META_PATH = os.path.join(PATH_DATA, "Metastases_old/")
+# OLD_META_PATH = os.path.join(PATH_DATA, "Metastases_old/")
 
 path_classif_lungs = os.path.join(PATH_DATA, "Classif_lungs")
 path_classif_metas = os.path.join(PATH_DATA, "Classif_metas")
 
 
-path_souris = os.path.join(PATH_CLASSIF, "Souris/")
-tab = pd.read_csv(os.path.join(PATH_CLASSIF, "Tableau_General.csv")).values
-tab_meta = pd.read_csv(os.path.join(PATH_META, "new_tab_meta.csv")).values
-path_img_classif = os.path.join(PATH_CLASSIF, "Images/")
+# path_souris = os.path.join(PATH_CLASSIF, "Souris/")
+# tab = pd.read_csv(os.path.join(PATH_CLASSIF, "Tableau_General.csv")).values
+# tab_meta = pd.read_csv(os.path.join(PATH_META, "new_tab_meta.csv")).values
+# path_img_classif = os.path.join(PATH_CLASSIF, "Images/")
 
-path_mask = os.path.join(PATH_LUNGS, "Masques/")
+# path_mask = os.path.join(PATH_LUNGS, "Masques/")
 path_img = os.path.join(PATH_LUNGS, "Images/")
 path_lab = os.path.join(PATH_LUNGS, "Labels/")
 
-path_gen_img = os.path.join(PATH_DATA, "Dataset_generated/Images/")
-path_gen_lab = os.path.join(PATH_DATA, "Dataset_generated/Labels/")
+# path_gen_img = os.path.join(PATH_DATA, "Dataset_generated/Images/")
+# path_gen_lab = os.path.join(PATH_DATA, "Dataset_generated/Labels/")
 
-path_masked_img = os.path.join(PATH_DATA, "masked/Images/")
-path_masked_lab = os.path.join(PATH_DATA, "masked/Labels/")
+# path_masked_img = os.path.join(PATH_DATA, "masked/Images/")
+# path_masked_lab = os.path.join(PATH_DATA, "masked/Labels/")
 
 # meta_path_mask = os.path.join(PATH_META, "Masques/")
-meta_path_img = os.path.join(PATH_META, "Images/")
-meta_path_lab = os.path.join(PATH_META, "Labels/")
+# meta_path_img = os.path.join(PATH_META, "Images/")
+# meta_path_lab = os.path.join(PATH_META, "Labels/")
 new_meta_path_img = os.path.join(NEW_PATH_META, "Images/")
 new_meta_path_lab = os.path.join(NEW_PATH_META, "Labels/")
-old_meta_path_img = os.path.join(OLD_META_PATH, "Images/")
-old_meta_path_lab = os.path.join(OLD_META_PATH, "Labels/")
+# old_meta_path_img = os.path.join(OLD_META_PATH, "Images/")
+# old_meta_path_lab = os.path.join(OLD_META_PATH, "Labels/")
 
 
 model_list = {
     "detection": DeepMetav4.models.model_detection.model_detection,
-    "detection_bn": DeepMetav4.models.model_detection.model_detection_bn,
-    "detection_stride": DeepMetav4.models.model_detection.model_detection_stride,
+    # "detection_bn": DeepMetav4.models.model_detection.model_detection_bn,
+    # "detection_stride": DeepMetav4.models.model_detection.model_detection_stride,
     "small++": DeepMetav4.models.small_plus_plus.small_plus_plus,
-    "unet": DeepMetav4.models.model_unet.unet,
-    "vgg16": DeepMetav4.models.model_vgg16.vgg16,
-    "resnet34": DeepMetav4.models.model_resnet.resnet34,
-    "resnet18": DeepMetav4.models.model_resnet.resnet18,
-    "resnet50": DeepMetav4.models.model_resnet.resnet50,
-    "resnetv2": DeepMetav4.models.model_resnet.resnetv2,
+    # "unet": DeepMetav4.models.model_unet.unet,
+    # "vgg16": DeepMetav4.models.model_vgg16.vgg16,
+    # "resnet34": DeepMetav4.models.model_resnet.resnet34,
+    # "resnet18": DeepMetav4.models.model_resnet.resnet18,
+    # "resnet50": DeepMetav4.models.model_resnet.resnet50,
+    # "resnetv2": DeepMetav4.models.model_resnet.resnetv2,
 }
 
 # Souris Test #
